3832befc8961eebe1da1a5e759910cc15188c678
Marco Carraro <carraromarco89@gmail.com>
2016-01-12 16:24:40 +0100

    Small fixes

--- src/main.cpp
+++ src/main.cpp
@@ -15,28 +15,33 @@ typedef sensor_msgs::PointCloud2 PointCloudT;
 
 namespace enc = sensor_msgs::image_encodings;
 
-void callback(const PointCloudT::ConstPtr& cloud1,
-              const PointCloudT::ConstPtr& cloud2,
+void callback(const PointCloudT::ConstPtr& cloud_dx,
+              const PointCloudT::ConstPtr& cloud_sx,
               const PointCloudT::ConstPtr& clouds,
-              const ImageConstPtr& image )
+              const ImageConstPtr& image_dx,
+              const ImageConstPtr& image_sx)
 {
   //std::cout << "Approximate Callback!" << std::endl;
-    cv_bridge::CvImageConstPtr cv_ptr = cv_bridge::toCvShare(image, enc::BGR8);
+    cv_bridge::CvImageConstPtr cv_ptr1 = cv_bridge::toCvShare(image_dx, enc::BGR8);
+    cv_bridge::CvImageConstPtr cv_ptr2 = cv_bridge::toCvShare(image_sx, enc::BGR8);
+
 
     std::cout << "Caught synchronized callback, saving to file ...";
 
     std::stringstream ss;
-    ss << "RGB" << image->header.stamp << ".png";
-
-    cv::imwrite(ss.str(), cv_ptr->image);
+    ss << "RGB_DX_" << image_dx->header.stamp << ".png";
+    cv::imwrite(ss.str(), cv_ptr1->image);
     ss.str(std::string());
-    ss << "CLOUD_DX_" << image->header.stamp << ".pcd";
-    pcl::io::savePCDFile(ss.str(),*cloud1);
+    ss << "RGB_SX_" << image_dx->header.stamp << ".png";
+    cv::imwrite(ss.str(), cv_ptr1->image);
     ss.str(std::string());
-    ss << "CLOUD_SX_" << image->header.stamp << ".pcd";
-    pcl::io::savePCDFile(ss.str(),*cloud2);
+    ss << "CLOUD_DX_" << image_dx->header.stamp << ".pcd";
+    pcl::io::savePCDFile(ss.str(),*cloud_dx);
     ss.str(std::string());
-    ss << "CLOUD_ZZ_" << image->header.stamp << ".pcd";
+    ss << "CLOUD_SX_" << image_dx->header.stamp << ".pcd";
+    pcl::io::savePCDFile(ss.str(),*cloud_sx);
+    ss.str(std::string());
+    ss << "CLOUD_ZZ_" << image_dx->header.stamp << ".pcd";
     pcl::io::savePCDFile(ss.str(),*clouds);
 
     std::cout << "Done" << std::endl;
@@ -49,15 +54,16 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "vision_node");
 
   ros::NodeHandle nh;
-  message_filters::Subscriber<PointCloudT> cloud1_sub(nh, "/cloud1", 1);
-  message_filters::Subscriber<PointCloudT> cloud2_sub(nh, "/cloud2", 1);
+  message_filters::Subscriber<PointCloudT> cloud1_sub(nh, "/cloud_dx", 1);
+  message_filters::Subscriber<PointCloudT> cloud2_sub(nh, "/cloud_sx", 1);
   message_filters::Subscriber<PointCloudT> cloud3_sub(nh, "/clouds", 1);
-  message_filters::Subscriber<Image> image_sub(nh, "/Kinect_left/rgb/image_color", 1);
+  message_filters::Subscriber<Image> image1_sub(nh, "/Kinect_right/rgb/image_color", 1);
+  message_filters::Subscriber<Image> image2_sub(nh, "/Kinect_left/rgb/image_color", 1);
 
-  typedef sync_policies::ApproximateTime<PointCloudT, PointCloudT, PointCloudT, Image> MySyncPolicy;
+  typedef sync_policies::ApproximateTime<PointCloudT, PointCloudT, PointCloudT, Image, Image> MySyncPolicy;
   // ApproximateTime takes a queue size as its constructor argument, hence MySyncPolicy(10)
-  Synchronizer<MySyncPolicy> sync(MySyncPolicy(100), cloud1_sub, cloud2_sub, cloud3_sub, image_sub);
-  sync.registerCallback(boost::bind(&callback, _1, _2, _3, _4));
+  Synchronizer<MySyncPolicy> sync(MySyncPolicy(100), cloud1_sub, cloud2_sub, cloud3_sub, image1_sub, image2_sub);
+  sync.registerCallback(boost::bind(&callback, _1, _2, _3, _4, _5));
 
   ros::spin();
 
